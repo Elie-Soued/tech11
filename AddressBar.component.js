8c9e8a2fd088123f5e4cbdab046ee8498e66257f
Elie Soued <eliegebransoued@gmail.com>
2021-10-18 19:44:15 +0200

    add a second api call to get the street per city

--- AddressBar.component.js
+++ AddressBar.component.js
@@ -54,17 +54,38 @@ class Address extends HTMLElement {
     this.shadowRoot.getElementById("zip").addEventListener(
       "change",
 
+      //First Api call to set The city and the country
       (e) => {
         let xmlReq = new XMLHttpRequest();
         xmlReq.open("GET", `http://api.zippopotam.us/DE/${e.target.value}`);
 
         xmlReq.onload = () => {
           const data = JSON.parse(xmlReq.response);
-
           this.shadowRoot.getElementById("city").value =
             data.places[0]["place name"];
 
           this.shadowRoot.getElementById("country").value = data.country;
+
+          //Second Api call to set The street per city
+          let http = new XMLHttpRequest();
+          let url = `https://cors-anywhere.herokuapp.com/https://www.postdirekt.de/plzserver/PlzAjaxServlet?autocomplete=plz&plz_city=${
+            this.shadowRoot.getElementById("city").value
+          }`;
+
+          http.open("POST", url, true);
+
+          http.setRequestHeader(
+            // "Access-Control-Allow-Origin",
+            "Content-type",
+            "application/x-www-form-urlencoded"
+          );
+
+          http.onreadystatechange = function () {
+            if (http.readyState == 4 && http.status == 200) {
+              console.log(JSON.parse(http.response));
+            }
+          };
+          http.send();
         };
 
         xmlReq.send();
