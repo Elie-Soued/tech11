d9b5e0ca1878d74724b70bc0e012bf29ac3a015d
Elie Soued <eliegebransoued@gmail.com>
2021-10-26 09:02:07 +0200

    update the script.js file

--- Component_Interaction/script.js
+++ Component_Interaction/script.js
@@ -1,11 +1,41 @@
-//Selecting the element from the DOM
-const validDate = document.getElementById("validDate");
-const debug = document.getElementById("debug");
-const debugContainer = document.getElementById("debugContainer");
-const test = document.getElementById("test");
-const household = document.getElementById("Household");
-const bicycle = document.getElementById("Bicycle");
+//Selecting the elements from the dom
+const myValidDate = document.getElementById("myValidDate");
+const myContract = document.getElementById("myContract");
 const textArea = document.getElementById("textArea");
+
+//Setting Event Listeners
+myContract.addEventListener("household", (e) => {
+  textArea.innerHTML = "The flat of the policy holder is 100 square meters";
+  delete data.contract.contractModules.BICYCLE;
+  if (!data.contract.contractModules.HOUSEHOLD) {
+    data.contract.contractModules.HOUSEHOLD = {
+      key: "HOUSEHOLD",
+      name: "Household Contents",
+      comments: "The flat of the policy holder is 100 square meters",
+    };
+  }
+  debug.innerText = JSON.stringify(data);
+});
+
+myContract.addEventListener("bicycle", (e) => {
+  textArea.innerHTML =
+    "The policyholder is happy to insure his new E-Bike also within the contract";
+  delete data.contract.contractModules.HOUSEHOLD;
+  if (!data.contract.contractModules.BICYCLE) {
+    data.contract.contractModules.BICYCLE = {
+      key: "BICYCLE",
+      name: "Bicycle",
+      comments:
+        "The policyholder is happy to insure his new E-Bike also within the contract",
+    };
+  }
+  debug.innerText = JSON.stringify(data);
+});
+
+myValidDate.addEventListener("send", (e) => {
+  data.contract.administrativeData.validFrom = e.detail.message;
+  debug.innerText = JSON.stringify(data);
+});
 
 //Defining the data that has to be in stringify initially
 const data = {
@@ -30,39 +60,3 @@ const data = {
 };
 
 debug.innerText = JSON.stringify(data);
-
-//Setting event listeners
-
-validDate.addEventListener("change", (e) => {
-  data.contract.administrativeData.validFrom = e.currentTarget.value;
-  debug.innerText = JSON.stringify(data);
-  validDate.value = "";
-});
-
-household.addEventListener("change", () => {
-  textArea.value = "The flat of the policy holder is 100 square meters";
-  delete data.contract.contractModules.BICYCLE;
-  if (!data.contract.contractModules.HOUSEHOLD) {
-    data.contract.contractModules.HOUSEHOLD = {
-      key: "HOUSEHOLD",
-      name: "Household Contents",
-      comments: "The flat of the policy holder is 100 square meters",
-    };
-  }
-  debug.innerText = JSON.stringify(data);
-});
-
-bicycle.addEventListener("change", () => {
-  textArea.value =
-    "The policyholder is happy to insure his new E-Bike also within the contract";
-  delete data.contract.contractModules.HOUSEHOLD;
-  if (!data.contract.contractModules.BICYCLE) {
-    data.contract.contractModules.BICYCLE = {
-      key: "BICYCLE",
-      name: "Bicycle",
-      comments:
-        "The policyholder is happy to insure his new E-Bike also within the contract",
-    };
-  }
-  debug.innerText = JSON.stringify(data);
-});
