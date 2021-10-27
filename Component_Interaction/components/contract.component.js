7dcd859f9da90a55094196fcb4b94426178beb6f
Elie Soued <eliegebransoued@gmail.com>
2021-10-27 16:21:27 +0200

    contract component restructure

--- Component_Interaction/components/contract.component.js
+++ Component_Interaction/components/contract.component.js
@@ -1,37 +1,46 @@
-customElements.define(
-  "app-contract",
-  class extends HTMLElement {
-    connectedCallback() {
-      this.innerHTML = `   
-        <div>
-        <form>
-          <p>Please select the type of contract:</p>
-           
-          <input
-            type="radio"
-            id="Household"
-            name="contractType"
-            value="Household"
-          />
-            <label for="Household">Household</label><br />
-  
-           
-          <input type="radio" id="Bicycle" name="contractType" value="Bicycle" />
-            <label for="Bicycle">Bicycle</label><br /> 
-        </form>
-      </div>`;
+const template = document.createElement("template");
 
-      Household.onchange = () => {
-        this.dispatchEvent(
-          new CustomEvent("household", { detail: { message: Household.value } })
-        );
-      };
+template.innerHTML = `   
+<div>
+<form>
+  <h3>Please select the type of contract:</h3>
+   
+  <input
+    type="radio"
+    id="Household"
+    name="contractType"
+    value="Household"
+  />
+    <label for="Household">Household</label><br />
 
-      Bicycle.onchange = () => {
-        this.dispatchEvent(
-          new CustomEvent("bicycle", { detail: { message: Bicycle.value } })
-        );
-      };
-    }
+   
+  <input type="radio" id="Bicycle" name="contractType" value="Bicycle" />
+    <label for="Bicycle">Bicycle</label><br /> 
+</form>
+</div>`;
+
+class Contract extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
-);
+
+  connectedCallback() {
+    const Household = this.shadowRoot.getElementById("Household");
+    const Bicycle = this.shadowRoot.getElementById("Bicycle");
+
+    Household.onchange = () => {
+      this.dispatchEvent(
+        new CustomEvent("household", { detail: { message: Household.value } })
+      );
+    };
+
+    Bicycle.onchange = () => {
+      this.dispatchEvent(
+        new CustomEvent("bicycle", { detail: { message: Bicycle.value } })
+      );
+    };
+  }
+}
+customElements.define("app-contract", Contract);
