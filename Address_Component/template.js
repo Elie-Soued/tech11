292744a59201cb5c40840c9f0b18afcbba69eaac
Elie Soued <eliegebransoued@gmail.com>
2021-11-17 16:44:33 +0100

    adding bootstrap to the project

--- Address_Component/template.js
+++ Address_Component/template.js
@@ -1,6 +1,9 @@
 const template = document.createElement("template");
 template.innerHTML = `
 
+<link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css"/>
+<link rel="stylesheet"href= "/node_modules/bootstrap/dist/js/bootstrap.min.js"/>
+
 <style>
 
 .smallSize { 
@@ -31,63 +34,79 @@ template.innerHTML = `
   opacity:0%;
 }
 
+
+
 </style>
 
-<form id="form">
-<p id="noData">Data not available</p>
-<p id="pinOffRange">Zip code should be between 01067 and 99998</p>
-<p id="invalidPin">Zip code is invalid</p>
-<div>
-  <label>ZIP</label>
-  <input 
-   type="number"
-   id="zip"
-   name="zip"
-   required />
-
+<div class="container">
+  <div class="row" id="firstRow">
+    <div class="col-xs-12">
+    <form id="form">
+      <p id="noData">Data not available</p>
+      <p id="pinOffRange">Zip code should be between 01067 and 99998</p>
+      <p id="invalidPin">Zip code is invalid</p>
+      <div>
+        <label>ZIP</label>
+        <input 
+          type="number"
+          id="zip"
+          name="zip"
+          class="form-control"
+          required />
+        
+        <label>City</label>
   
-
-  <label>City</label>
-  <input type="text"
-   class="smallSize"
-   id="city"
-   name="city"
-   required />
+        <input type="text"
+        class="smallSize"
+        id="city"
+        name="city"
+        required />
   
-   <label>District</label>
-  <select 
-   class="mediumSize"
-    id="district"
-    name="district"
-    required></select>
+        <label>District</label>
+  
+        <select 
+          class="mediumSize"
+          id="district"
+          name="district"
+          required>
+        </select>
+      </div>
+
+      <div>
+        <label>Street</label>
+        <select
+          class="mediumSize"
+          id="street"
+          name="street"
+          required>
+        </select>
+      
+        <label>House Number</label>
+  
+        <input 
+          type="number"
+          id="houseNumber"
+          name="houseNumber"
+          required />
+      </div>
+
+      <div>
+        <label>Country</label>
+        <input
+        id="country"
+        name="country"
+        required />
+      </div>
+  </form>  
+    </div>
+  </div>
 </div>
 
-<div>
-  <label>Street</label>
-  <select
-   class="mediumSize"
-   id="street"
-   name="street"
-   required></select>
-  <label>House Number</label>
-  
-  <input 
-  type="number"
-   id="houseNumber"
-   name="houseNumber"
-   required />
-</div>
+<button type ="button" class="btn btn-success" id="submit" disabled>Print</button>
+<button type="button" class=" btn btn-danger" id="reset">Reset</button>
 
-<div>
-  <label>Country</label>
-  <input
-   id="country"
-    name="country"
-    required />
-</div>
-</form>
-<button id="submit" disabled>Info</button>
-<button id="reset">Reset</button>
+
+
 `;
 
 export { template };
